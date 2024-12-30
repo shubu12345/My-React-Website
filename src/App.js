913b37dfc2fb231425e4a5c3f5b796beb98a62e2
LL\shushind <shubham.shinde@publicisapient.com>
2024-12-30 19:13:18 +0530

    Implemented new features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import Header from './components/Header.js'
-import './Styles.css';
-import airoplne from './assets/images/plane.gif';
-import profile from './assets/images/shubham.jpeg';
-// import resume from './assets/images/resume.png';
-import project1 from './assets/images/project1.png';
-import project2 from './assets/images/project2.png';
-import About from './components/About.js';
-// import Download from './components/Download.js';
-import Work from './components/Work.js';
-import Title from './components/Title.js';
-import Support from './components/Support.js';
+import Header from "./components/Header.js";
+import "./Styles.css";
+import airoplne from "./assets/images/plane.gif";
+// import profile from "./assets/images/shubham.jpeg";
+import ss from "./assets/images/profile.png";
+import project1 from "./assets/images/project1.png";
+import project2 from "./assets/images/project2.png";
+import About from "./components/About.js";
+import Work from "./components/Work.js";
+import Title from "./components/Title.js";
+import Support from "./components/Support.js";
 
 function App() {
   return (
     <div>
-      <Header imgUrl={airoplne}/>
-      <About  imgUrl={profile}/>
+      <Header imgUrl={airoplne} />
+      <About imgUrl={ss} />
       {/* <Download imgUrl={resume}/> */}
       <Title />
-      <Work id='p1' imgUrl={project1}/>
-      <Support id='p2' imgUrl={project2}/>
+      <Work id="p1" imgUrl={project1} />
+      <Support id="p2" imgUrl={project2} />
     </div>
   );
 }
